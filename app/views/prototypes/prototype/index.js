2304558ad0502321125e8fe889690816884c98ea
Colin Rotherham <work@colinr.com>
2019-04-17 15:20:00 +0100

    Make PYCA-1146 live prototype

--- app/views/prototypes/prototype/index.js
+++ app/views/prototypes/prototype/index.js
@@ -3,6 +3,7 @@ const router = express.Router()
 
 // Local dependencies
 const countries = require('../../../data/location-autocomplete')
+const decisions = require('../../../data/decision')
 const rightsEEA = require('../../../data/right-to-reside-eea')
 const rightsNonEEA = require('../../../data/right-to-reside-non-eea')
 const rightsList = rightsEEA.list().concat(rightsNonEEA.list())
@@ -19,13 +20,13 @@ router.all('/', (req, res) => {
  * Redirects to first question
  */
 router.all('/questions', (req, res) => {
-  return res.redirect('./questions/find-decision')
+  return res.redirect('./questions/find-decision-to-do')
 })
 
 /**
- * Question: Can you locate the decision?
+ * Question: Is the HRT decision recorded in the history?
  */
-router.all('/questions/find-decision', (req, res) => {
+router.all('/questions/find-decision-history', (req, res) => {
   const submitted = req.body
 
   // Decision found
@@ -38,7 +39,44 @@ router.all('/questions/find-decision', (req, res) => {
     return res.redirect('../outcome/do-not-clear')
   }
 
-  res.render(`${__dirname}/views/questions/find-decision`)
+  res.render(`${__dirname}/views/questions/find-decision-history`)
+})
+
+/**
+ * Question: Is the HRT decision recorded in the to-do?
+ */
+router.all('/questions/find-decision-to-do', (req, res) => {
+  const submitted = req.body
+
+  // Decision found
+  if (submitted.findDecisionToDo === 'yes') {
+    return res.redirect('./decision')
+  }
+
+  // Decision not found
+  if (submitted.findDecisionToDo === 'no') {
+    return res.redirect('./find-decision-history')
+  }
+
+  res.render(`${__dirname}/views/questions/find-decision-to-do`)
+})
+
+/**
+ * Question: What’s the current decision recorded in the to-do?
+ */
+router.all('/questions/decision', (req, res) => {
+  const submitted = req.body
+  const saved = req.session.data
+
+  // Check right to reside
+  if (submitted.rightToReside) {
+    return res.redirect('./check-right-to-reside')
+  }
+
+  res.render(`${__dirname}/views/questions/decision`, {
+    items: decisions.list(saved.rightToReside),
+    source: `/data/decision/source/decision-graph.json`
+  })
 })
 
 /**
@@ -75,25 +113,47 @@ router.all('/questions/right-to-reside/:type', (req, res) => {
   // Check if routing EEA or Non-EEA
   const type = req.params.type
 
+  // Only accept valid types
+  if (!['eea', 'non-eea'].includes(type)) {
+    return res.redirect('../')
+  }
+
   // Check right to reside
   if (submitted.rightToReside) {
-    if (['student', 'self_sufficient'].includes(submitted.rightToReside)) {
-      return res.redirect('../../outcome/do-not-clear')
-    }
-
-    if (['permanent_right_to_reside'].includes(submitted.rightToReside)) {
-      return res.redirect('../../outcome/clear')
-    }
-
-    // Normalise with hyphens for URL
-    const right = submitted.rightToReside.replace(/_/g, '-')
-    return res.redirect(`../confirm-change/${right}`)
+    return res.redirect('../check-right-to-reside')
   }
 
   res.render(`${__dirname}/views/questions/right-to-reside`, {
     items: type === 'eea' ? rightsEEA.list(saved.rightToReside) : rightsNonEEA.list(saved.rightToReside),
     source: `/data/right-to-reside-${type}/source/right-to-reside-${type}-graph.json`
   })
+})
+
+/**
+ * Handle right to reside redirect
+ */
+router.all('/questions/check-right-to-reside', (req, res) => {
+  const saved = req.session.data
+
+  if (saved.rightToReside) {
+    if (saved.rightToReside.match(/(student|self_sufficient|(?<!non_)eea_(indefinite|limited)_leave_to_remain)$/g)) {
+      return res.redirect('../outcome/do-not-clear')
+    }
+
+    if (saved.rightToReside.match(/(british|permanent_right_to_reside)$/g)) {
+      return res.redirect('../outcome/clear')
+    }
+
+    // Normalise: Remove prefix, use hyphens for URL
+    const right = saved.rightToReside
+      .replace(/^(non_)?eea_/, '')
+      .replace(/_/g, '-')
+
+    // Redirect to change of circumstances
+    return res.redirect(`./confirm-change/${right}`)
+  }
+
+  res.redirect('./find-decision-to-do')
 })
 
 /**
@@ -119,6 +179,13 @@ for (let { value: right } of rightsList) {
     res.render(`${__dirname}/views/questions/confirm-change/${right}`)
   })
 }
+
+/**
+ * Questions
+ */
+router.all('/questions/:question', (req, res) => {
+  res.render(`${__dirname}/views/questions/${req.params.question}`)
+})
 
 /**
  * Outcomes
